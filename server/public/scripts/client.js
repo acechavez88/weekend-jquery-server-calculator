93a5498d2577bda0ec13c9b89868bc5dcf26fa76
Abram Chavez <abram.chavez820@gmail.com>
2021-06-25 16:07:28 -0500

    created notes with object for input

---
+++ server/public/scripts/client.js
@@ -0,0 +1,14 @@
+$(document).on (onReady){
+    console.log('Im ready');
+
+$(.operatorBtn).on('click', () =>{
+})
+
+}
+
+packet: {
+    number:"",
+    number2:"",
+    operator:"", 
+    answer:","
+}
