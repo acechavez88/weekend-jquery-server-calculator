fc1f8244135d61342ba381fe8607331d4dd21185
Abram Chavez <abram.chavez820@gmail.com>
2021-06-26 13:57:09 -0500

    created app.use w/ express & urlencoded true, json

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
 
+// This must be added before GET & POST routes.
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
 // creating an object of input for GET
 packet: {
     number:"",
